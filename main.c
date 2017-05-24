536166caba80777071aa2ac4a487ea07d3aef556
Francisco <ftolaba@itba.edu.ar>
2017-05-24 18:42:21 -0300

    primer commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: frantolaba
+ *
+ * Created on 24 de mayo de 2017, 18:28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(void) 
+{
+    
+    return (EXIT_SUCCESS);
+}
+
